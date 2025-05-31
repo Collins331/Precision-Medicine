5542669207ffeff43a1dfbca04c86311388c4341
Collins <ochiengcollinsoduor2@gmail.com>
2025-06-01 00:29:50 +0300

    R badics

---
+++ Programming Basics/R/Basics.r
@@ -0,0 +1,11 @@
+print("I am learning R")
+
+# To remove quotes from printed statetemenst, quote paramter is assigned to fqlse
+print("Hello there", quote=FALSE)
+
+# Or use noquote to print the text
+print(noquote("R learning is interesting"))
+
+# Define a variable
+name <- "Collins"
+print(name)
