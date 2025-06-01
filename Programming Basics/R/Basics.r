b685a9c4ec541ba515cb4eb8ad7b009dad64911b
Collins <ochiengcollinsoduor2@gmail.com>
2025-06-01 23:53:31 +0300

    R vector data structure

--- Programming Basics/R/Basics.r
+++ Programming Basics/R/Basics.r
@@ -9,3 +9,36 @@ print(noquote("R learning is interesting"))
 # Define a variable
 name <- "Collins"
 print(name)
+
+# Concatinate character type
+names <- paste("Collins", "Ochieng", sep = " ")
+
+# concatenate characters & variables
+current_year <- 2025
+year_of_birth <- 2003
+
+age <- current_year - year_of_birth
+
+intro <- cat(current_year, "-", year_of_birth, "=", age, sep = " ")
+print(intro)
+
+# Arithmetic operations
+#1. Addition
+money <- 231 + 134
+#2. Substraction
+years <- 2025 - 2003
+
+#3. Multiplication
+salary <- 1.25 * 32500
+
+#4. Divisiom
+marks <- 100 / 1.25
+
+#5. Exponential
+pow <- 4^2
+
+print(money)
+print(years)
+print(salary)
+print(marks)
+print(pow)
---
+++ Programming Basics/R/Vectors.R
@@ -0,0 +1,20 @@
+# Vectors - is a one-dimensional sequence of data elements, of the same type, stored in a sequence
+# Instantiate vector, using c() function -> combine
+
+lucky_nums <- c(2, 4, 7, 3, 8)
+names <- c('Collins', 'Oduor', 'Ochieng', 'Othieno')
+print(lucky_nums, quote=FALSE)
+print(names, quote=FALSE)
+
+# Determine the type of data in a vector
+print(typeof(names))
+
+# Sequence of numbers
+one_10 <- 1:10
+print(one_10)
+
+# Also create sequence of numbers using `seq()`
+one_20 <- seq(20)
+print(one_20)
+ 
+
